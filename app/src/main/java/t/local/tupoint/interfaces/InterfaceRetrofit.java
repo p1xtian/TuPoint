fd5e41b9f73977c2ab42e8b4726bc6d57ccc75b6
Christian Mercado <cmercado@ipsec.com.pe>
2019-04-09 20:12:31 -0500

    201904092012

--- app/src/main/java/t/local/tupoint/interfaces/InterfaceRetrofit.java
+++ app/src/main/java/t/local/tupoint/interfaces/InterfaceRetrofit.java
@@ -6,6 +6,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 import t.local.tupoint.models.Admin;
 
 public interface InterfaceRetrofit {
@@ -16,6 +17,13 @@ public interface InterfaceRetrofit {
     @GET("admins")
     Call<List<Admin>> ListAdmins();
 
+    //Login
+    @GET("admins/email/{email}}")
+    Call<Admin> FindByEmail(@Path("email") String email);
+
+    //Login
+    @GET("admins/ruc/{ruc}}")
+    Call<Admin> FindByRUC(@Path("ruc") String ruc);
 
     //Save Admin
     @POST("registerAdmin")
